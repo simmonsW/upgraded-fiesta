4e89820f940c9b0a3114f3e62b35a35e64f62930
Wyatt Simmons <simmonsw182@gmail.com>
2021-03-05 17:47:39 -0800

    added password length and character choice checks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,37 @@ function generatePassword() {
   var passwordLength = prompt("What length would you like your password to be? Choose anywhere from 8 - 128.");
   console.log(passwordLength);
 
+  // check password length
+  if (passwordLength < 8) {
+    alert("That is too short. please choose between 8 - 128.");
+    generatePassword();
+    return;
+  }
+  else if (passwordLength > 128) {
+    alert("That is too long. please choose between 8 - 128.");
+    generatePassword();
+    return;
+  }
+
   alert('Please select which characters to use.');
 
   var lowerCase = confirm("Use lowercase letters?");
   console.log(lowerCase);
 
-  var uppperCase = confirm("Use uppercase letters?");
-  console.log(uppperCase);
+  var upperCase = confirm("Use uppercase letters?");
+  console.log(upperCase);
 
   var numbers = confirm("Use numbers 0 - 9?");
   console.log(numbers);
 
   var specialCharacters = confirm("Use special characters?");
   console.log(specialCharacters);
+
+  if (!lowerCase && !upperCase && !numbers && !specialCharacters) {
+    alert("you must select at least one character type.")
+    generatePassword();
+    return;
+  }
 }
 
 // Get references to the #generate element
