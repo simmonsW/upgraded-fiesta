ba27b8030355fa6477cb1d8bd7e7ee52816a2a34
Wyatt Simmons <simmonsw182@gmail.com>
2021-03-05 17:12:19 -0800

    fixed links and initial user prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
 // Assignment code here
+function generatePassword() {
+  var passwordLength = prompt("What length would you like your password to be? Choose anywhere from 8 - 128.");
+  console.log(passwordLength);
 
+  alert('Please select which characters to use.');
+
+  var lowerCase = confirm("Use lowercase letters?");
+  console.log(lowerCase);
+
+  var uppperCase = confirm("Use uppercase letters?");
+  console.log(uppperCase);
+
+  var numbers = confirm("Use numbers 0 - 9?");
+  console.log(numbers);
+
+  var specialCharacters = confirm("Use special characters?");
+  console.log(specialCharacters);
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
